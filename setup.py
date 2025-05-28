4719b010acd99dd3686bee1235d315897e3edfa9
Tal Wrii <talwrii@gmail.com>
2025-05-28 15:01:57 +0200

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import distutils.core
 
 setuptools.setup(
     name='gh-things',
-    version="1.1.2",
+    version="1.1.3",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
@@ -12,7 +12,7 @@ setuptools.setup(
     license='GPLv3',
     keywords='github,gh,JSON,objects',
     url='https://github.com/talwrii/gh-things',
-    packages=["gh_objects"],
+    packages=["gh_things"],
     long_description=open('README.md').read(),
     entry_points={
         'console_scripts': ['gh-things=gh_things.main:main']
