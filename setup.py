ab4d9f5ad6595ed335f858038a11c7f7ba5d260d
Tal Wrii <talwrii@gmail.com>
2025-05-28 14:59:16 +0200

    bump version rename.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import distutils.core
 
 setuptools.setup(
     name='gh-things',
-    version="1.1.0",
+    version="1.1.1",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
     description='Companion app for the Github CLI gh to fetch machine readable information about Github objects',
     license='GPLv3',
     keywords='github,gh,JSON,objects',
-    url='https://github.com/talwrii/gh-objects',
+    url='https://github.com/talwrii/gh-things',
     packages=["gh_objects"],
     long_description=open('README.md').read(),
     entry_points={
